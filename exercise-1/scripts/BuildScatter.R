8b54fa1a00f9849dea17477f22829e0a08ecf92b
Chianson Siu <chanmancu@gmail.com>
2017-11-14 13:57:52 -0800

    Add scatter plot function

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,9 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+
+library(ggplot2)
+BuildScatterPlot <- function(data, x.axis, y.axis, color, title="Title", x.label="X Title", y.label="Y Title") {
+  plot <- ggplot(data = data) + geom_point(aes(x = x.axis, y = y.axis, color = color)) + ggtitle(title) + xlab(x.label) + ylab(y.label) 
+  return(plot)
+}
